bd5c8ffe2efcafc228a3d46addc39367ab6fcb6f
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-05-20 15:54:26 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,171 @@
+import React, {useState} from 'react';
+import {StatusBar, StyleSheet, View, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  Container,
+  Content,
+  Header,
+  Body,
+  Card,
+  H1,
+  H3,
+  Button,
+  Title,
+} from 'native-base';
+import Icons from './components/Icons';
+import Snackbar from 'react-native-snackbar';
+
+const itemArray = new Array(9).fill('empty');
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState('');
+
+  const changeItem = itemNumber => {
+    // change circle to cross and vise versa
+    if (winMessage) {
+      return Snackbar.show({
+        text: winMessage,
+        backgroundColor: '#000',
+        textColor: '#FFF',
+      });
+    }
+    if (itemArray[itemNumber] == 'empty') {
+      itemArray[itemNumber] = isCross ? 'cross' : 'circle';
+      setIsCross(!isCross);
+    } else {
+      return Snackbar.show({
+        text: 'Position already filled!',
+        backgroundColor: 'red',
+        textColor: '#FFF',
+      });
+    }
+    checkIsWinner();
+  };
+
+  const reloadGame = () => {
+    // reset the game
+    setIsCross(false);
+    setWinMessage('');
+    itemArray.fill('empty', 0, 9);
+  };
+
+  const checkIsWinner = () => {
+    //  checking  winner of the game
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== 'empty'
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[3] !== 'empty' &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5]
+    ) {
+      setWinMessage(`${itemArray[3]} won`);
+    } else if (
+      itemArray[6] !== 'empty' &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[6]} won`);
+    } else if (
+      itemArray[0] !== 'empty' &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[1] !== 'empty' &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7]
+    ) {
+      setWinMessage(`${itemArray[1]} won`);
+    } else if (
+      itemArray[2] !== 'empty' &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
+    } else if (
+      itemArray[0] !== 'empty' &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[2] !== 'empty' &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
+    }
+  };
+
+  return (
+    <Container style={{backgroundColor: '#333945', padding: 5}}>
+      <Header>
+        <Body>
+          <Title>GP TicTacToe</Title>
+        </Body>
+      </Header>
+      <Content>
+        <View style={styles.grid}>
+          {itemArray.map((item, index) => (
+            <TouchableOpacity
+              style={styles.box}
+              key={index}
+              onPress={() => {
+                changeItem(index);
+              }}>
+              <Card style={styles.card}>
+                <Icons name={item} />
+              </Card>
+            </TouchableOpacity>
+          ))}
+        </View>
+        {winMessage ? (
+          <View>
+            <H1 style={styles.message}>{winMessage}</H1>
+            <Button onPress={reloadGame} warning bordered block rounded>
+              <Text>Reload Game</Text>
+            </Button>
+          </View>
+        ) : (
+          <H3 style={styles.message}>{isCross ? 'cross' : 'circle'} turns</H3>
+        )}
+      </Content>
+    </Container>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  grid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+  },
+  box: {
+    width: '33%',
+    marginBottom: 0,
+  },
+  card: {
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 150,
+  },
+  message: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    color: '#FFF',
+    marginTop: 20,
+    backgroundColor: '#4652B3',
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native Tic Tac Toe
